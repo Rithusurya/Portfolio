404b90208821e0c3ba77b22420d8c926b459d960
RithuSurya <146659491+Rithusurya@users.noreply.github.com>
2024-02-27 22:40:47 +0530

    Add files via upload
    my files

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".auto-type",
+    {
+        strings: ["Software developer", "designer", "Freelancer"],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true
+    })
